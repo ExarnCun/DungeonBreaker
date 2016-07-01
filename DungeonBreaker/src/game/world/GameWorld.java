ff4731e8f26e77f3bd700f579b111f635e914034
ExarnCun <ExarnCun@web.de>
2016-07-01 15:52:25 +0200

    First Release

---
+++ DungeonBreaker/src/game/world/GameWorld.java
@@ -0,0 +1,29 @@
+package game.world;
+
+import java.awt.Graphics2D;
+
+import world.World;
+
+public class GameWorld extends World{
+
+	private static final long serialVersionUID = 955592560605788155L;
+
+	@Override
+	public void postInit() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void Tick() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void render(Graphics2D g) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
--- DungeonBreaker/src/world/World.java
+++ DungeonBreaker/src/world/World.java
@@ -1,5 +1,12 @@
 package world;
 
+import java.util.Calendar;
+import java.util.Date;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
+import java.io.ObjectInputStream;
+import javax.swing.JFrame;
 
 /**
  * 
@@ -8,7 +15,23 @@ package world;
  * @author ExarnCun
  *
  */
-public abstract class World {
+public abstract class World extends JFrame {
+	
+
+
+	/**
+	 * Used for Object {@link ObjectInputStream} etc.
+	 */
+	private static final long serialVersionUID = 7060784873153920329L;
+
+
+	/**
+	 * Separated Thread for Ticking
+	 * 
+	 * @see Ticker
+	 */
+	public Ticker TickThread;
+	
 	
 	/**
 	 * Ticks per second as double
@@ -19,12 +42,90 @@ public abstract class World {
 	
 	
 	/**
-	 * Tick-Method.
-	 * Executed {@link #TPS} times per second
+	 * Separated Thread for Rendering
+	 * 
+	 * @see Ticker
 	 */
-	public void Tick(){
+	public Renderer RenderThread;
+	
+	
+	/**
+	 * Renderings per second as double
+	 * 
+	 * @see #paint(Graphics)
+	 */
+	public double RPS = 64;
+	
+	/**
+	 * Background image of the game
+	 */
+	public BufferedImage GBackground;
+	
+	/**
+	 * next image to draw on JFrame
+	 */
+	public BufferedImage NextFrame;
+	
+	/**
+	 * Graphics of the game
+	 */
+	public Graphics2D GGraphics;
+	
+	/**
+	 * Horizontal and vertical resolution of the game
+	 */
+	public int Xresolution, Yresolution;
+	
+	
+	/**
+	 * Initializes the world
+	 */
+	public void Init(){
+		TickThread = new Ticker(TPS, this);
+		RenderThread = new Renderer(RPS, this);
+		
+		//TODO: init 'GBackground' and 'NextFrame'
+		
+		GGraphics = NextFrame.createGraphics();
+		
+		//TODO: add stuff
+		
+		postInit();
+	}
+	
+	
+	
+	@Override
+	public void paint(Graphics g){
+		
+		GGraphics.drawImage(GBackground, 0, 0, getWidth(), getHeight(), null); //Draw Background
+		
+		render(GGraphics);
+		
+		g.drawImage(NextFrame, 0, 0, getWidth(), getHeight(), null); // Draw next Frame on JFrame
 		
 	}
+	
+	
+	//ABSTRACT METHODS
+	
+	/**
+	 * invoked every time after the {@link #Init() Init-method} is invoked
+	 */
+	public abstract void postInit();
+	
+	/**
+	 * <b>Tick-Method.</b><br>
+	 * Invoked {@link #TPS} times per second
+	 */
+	public abstract void Tick();
+	
+	/**
+	 * invoked every time when the {@link #paint(Graphics) paint-method} is invoked
+	 * 
+	 * @param g the Graphics of the game
+	 */
+	public abstract void render(Graphics2D g);
 	
 }
 
@@ -77,9 +178,129 @@ class Ticker implements Runnable{
 		this.Target = Target;
 	}
 
+	
+	/**
+	 * 
+	 * <b>Basic GameLoop (TickLoop)</b>
+	 * 
+	 * @author ExarnCun
+	 */
 	@Override
 	public void run() {
-		
+		Date a = Calendar.getInstance().getTime();
+		Date b = Calendar.getInstance().getTime();
+		double TargetTimeout = 1000 / TPS;
+		double Timeout = 0;
+		while(active){
+			Target.Tick();
+			if(Timeout > 0){
+				try{
+					Thread.sleep((long)Timeout);
+				} catch(InterruptedException e){
+					
+				}
+			}
+			if(Timeout <= 0){
+				//TODO: Add information about skipped Ticks
+			}
+			while(Timeout <= 0){
+				Target.Tick();
+				Timeout += TargetTimeout;
+			}
+			b = Calendar.getInstance().getTime();
+			Timeout += TargetTimeout + a.getTime() - b.getTime();
+			a = Calendar.getInstance().getTime();
+		}
+	}
+	
+}
+
+/**
+ * A separated Thread for calling the Render-method of the World
+ */
+class Renderer implements Runnable{
+	
+	/**
+	 * Renderings per second
+	 */
+	double RPS;
+	
+	/**
+	 * as long as this boolean is 'true' the Render-Thread is running
+	 */
+	boolean active;
+	
+	/**
+	 * Instance which's Render-method should be called
+	 */
+	World Target;
+	
+	/**
+	 * Starts the Render-Thread
+	 */
+	public void start(){
+		if(!active){
+			new Thread(this).start();
+		}
+	}
+	
+	/**
+	 * Stops the Render-Thread
+	 */
+	public void stop(){
+		active = false;
+	}
+	
+	
+	/**
+	 * 
+	 * <b>Constructor</b>
+	 * 
+	 * @param RPS Renderings per second ( how often the Render-method of the World should be called per second)
+	 * @param Target World, which's Render-method should be called
+	 */
+	public Renderer(double RPS, World Target){
+		this.RPS = RPS;
+		this.Target = Target;
+	}
+
+	
+	/**
+	 * 
+	 * <b>Basic RenderLoop</b>
+	 * 
+	 * @author ExarnCun
+	 */
+	@Override
+	public void run() {
+		Date a = Calendar.getInstance().getTime();
+		Date b = Calendar.getInstance().getTime();
+		double TargetTimeout = 1000 / RPS;
+		double Timeout = 0;
+		while(active){
+			try {
+				Target.paint(Target.getGraphics());
+			} catch (Exception e) {
+				// TODO: handle exception
+			}
+			if(Timeout > 0){
+				try{
+					Thread.sleep((long)Timeout);
+				} catch(InterruptedException e){
+					
+				}
+			}
+			if(Timeout <= 0){
+				//TODO: Add information about skipped Ticks
+			}
+			while(Timeout <= 0){
+				Target.paint(Target.getGraphics());
+				Timeout += TargetTimeout;
+			}
+			b = Calendar.getInstance().getTime();
+			Timeout += TargetTimeout + a.getTime() - b.getTime();
+			a = Calendar.getInstance().getTime();
+		}
 	}
 	
 }
